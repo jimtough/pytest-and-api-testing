3a06704bff9ae5fbde9d7a2e0fe4ddc43f8fc020
adultmalehuman <adultmalehuman@gmail.com>
2022-09-19 12:35:37 -0300

    using a fixture function with a test class

--- pytest_tutorial/my_tests/test_suite_2_with_class.py
+++ pytest_tutorial/my_tests/test_suite_2_with_class.py
@@ -4,14 +4,25 @@ import pytest
 pytestmark = [pytest.mark.fe, pytest.mark.slow]
 
 
+# setting 'module' scope will make pytest only execute this method once
+@pytest.fixture(scope="module")
+def also_my_setup():
+    print("")
+    print(">>>> ALSO MY SETUP <<<<")
+    return {'id': 20, 'name': 'Meeeee'}
+
+
 # mark every test method in this class as 'smoke'
 @pytest.mark.smoke
+@pytest.mark.abc
 class TestCheckout(object):
 
-    def test_checkout_as_guest(self):
+    def test_checkout_as_guest(self, also_my_setup):
         print("checkout as guest")
         print("Class: 111111")
+        print("Name: {}".format(also_my_setup.get('name')))
 
-    def test_checkout_with_existing_user(self):
+    def test_checkout_with_existing_user(self, also_my_setup):
         print("checkout with existing user")
         print("Class: 2222222222")
+        print("Name: {}".format(also_my_setup.get('name')))
