5ec54b82ebd7db6bd20912e6d4f9b59ae0efeb3a
adultmalehuman <adultmalehuman@gmail.com>
2022-09-19 11:23:22 -0300

    added two trivial PyTest examples

---
+++ pytest_tutorial/my_tests/test_suite_1.py
@@ -0,0 +1,12 @@
+
+
+
+
+def test_login_page_valid_user():
+    print("Logging in with valid user...")
+    print("Function: xxxxxxxx")
+
+def test_login_page_wrong_password():
+    print("Logging in with incorrect password..")
+    print("Function: yyyyyyyy")
+    # assert 1 == 2, "one is not equal to two"
