38aed775e73cac0ca0137cfa10bada9015150fcb
adultmalehuman <adultmalehuman@gmail.com>
2022-09-28 10:26:26 -0300

    added package for helper functions to be used in unit tests

--- my_apitest/test/customers/test_create_customers_smoke.py
+++ my_apitest/test/customers/test_create_customers_smoke.py
@@ -1,16 +1,21 @@
 import pytest
 import logging as logger
 
+from my_apitest.src.utilities.generic_utils import generate_random_email_and_password
+
 
 # instructor wants to name this "test case id 29" for some reason. Why 29?? whatever...
 @pytest.mark.tcid29
 def test_create_customer_only_email_and_password():
 
     logger.info("TEST: Create new customer with email and password only.")
-    email = ""
-    password = ""
+    rand_info = generate_random_email_and_password()
+    email = rand_info['email']
+    password = rand_info['password']
+    logger.debug("generated test values | email: [%s] | password: [%s]", email, password)
 
     # create payload
+    payload = {'email': email, 'password': password}
 
     # make the call
 
